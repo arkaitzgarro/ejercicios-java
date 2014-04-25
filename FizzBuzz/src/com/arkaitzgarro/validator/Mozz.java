f6cc8c1961d0243baea17a20d8439e255f1ffb85
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-04-25 10:40:54 +0200

    Añadir clase NumberNotValidException

--- FizzBuzz/src/com/arkaitzgarro/validator/Mozz.java
+++ FizzBuzz/src/com/arkaitzgarro/validator/Mozz.java
@@ -1,5 +1,6 @@
 package com.arkaitzgarro.validator;
 
+import com.arkaitzgarro.exception.NumberNotValidException;
 import com.arkaitzgarro.interfaces.IValidator;
 
 public class Mozz extends BaseValidator implements IValidator {
@@ -8,7 +9,9 @@ public class Mozz extends BaseValidator implements IValidator {
 		this.setOutput("Buzz");
 	}*/
 	
-	public boolean validate(int num) {
+	public boolean validate(int num) throws NumberNotValidException {
+		this.validateException(num);
+		
 		return num % 7 == 0;
 	}
 }
