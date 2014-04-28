ccb83d8c28faaaf5ba5352bc0db38b6f6fc6aad2
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-04-28 12:17:34 +0200

    Añadir excepción NumberNotValid

---
+++ FizzBuzz/src/com/arkaitzgarro/exception/NumberNotValidException.java
@@ -0,0 +1,11 @@
+package com.arkaitzgarro.exception;
+
+public class NumberNotValidException extends Exception {
+
+	private static final long serialVersionUID = -1472612734703147141L;
+
+	public NumberNotValidException(String message) {
+		super(message);
+	}
+
+}
