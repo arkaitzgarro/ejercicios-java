bd0d67e12ca7dbb6ae199455480d4e836adc7652
Arkaitz Garro <hola@arkaitzgarro.com>
2014-12-08 12:23:28 +0100

    Añadir operaciones básicas JDBC

--- JDBC/src/com/arkaitzgarro/jdbc/test/FilmTest.java
+++ JDBC/src/com/arkaitzgarro/jdbc/test/FilmTest.java
@@ -1,7 +1,10 @@
 package com.arkaitzgarro.jdbc.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -29,6 +32,22 @@ public class FilmTest {
 
 		assertTrue(film instanceof Film);
 		assertEquals(1, film.getId());
-	}
 
+		Film newFilm = FilmFactory.create();
+		newFilm.setTitle("Interstellar");
+		newFilm.setDescription("A group of explorers must travel beyond our solar system in search of a planet that can sustain life. The crew of the Endurance are required to think bigger and go further than any human in history as they embark on an interstellar voyage, into the unknown.");
+		newFilm.setYear(new Date());
+
+		assertTrue(FilmRepository.addFilm(newFilm));
+		assertNotEquals(0, newFilm.getId());
+
+		newFilm.setTitle("Interstellar 2014");
+		assertTrue(FilmRepository.updateFilm(newFilm));
+
+		Film otherFilm = FilmRepository.findOneById(newFilm.getId());
+		assertEquals(newFilm.getId(), otherFilm.getId());
+		assertEquals("Interstellar 2014", otherFilm.getTitle());
+
+		assertTrue(FilmRepository.removeFilm(newFilm));
+	}
 }
